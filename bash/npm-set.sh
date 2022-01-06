3f1d356ddd01805e871f3dc1d55d00b94d61ca71
Darryl-Medirecords <blackwind757@gmail.com>
2022-01-06 15:16:51 +0700

    ci: put secret github token to ci.yml file

--- bash/npm-set.sh
+++ bash/npm-set.sh
@@ -1,5 +1,5 @@
 npm config set always-auth true
-npm config set //npm.pkg.github.com/:_authToken ${{ secrets.GH_PACKAGE_TOKEN }}   
+npm config set //npm.pkg.github.com/:_authToken $GH_PACKAGE_TOKEN
 npm config set @BlackWind57:registry https://npm.pkg.github.com
 npm set email blackwind757@gmail.com
 git config --global user.email "cristo1494@yahoo.com"
